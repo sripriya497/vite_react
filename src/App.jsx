15c9e50cbb967a69b16d0ea46533c59480ea1a59
sripriya497 <sripriyadharshini497@gmail.com>
2023-09-30 21:24:02 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "@/Components/Navbar.jsx"
+import Main from "@/Components/Main.jsx"
+export default function App() {
+    return (
+        <div classname = 'Container'>
+            < Navbar />
+            < Main />
+        </div>
+    );
+    }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="../../image/React-icon.svg" width='50px' />
+            <h3>ReactFacts</h3>
+            <h4>Project 1</h4>
+        </nav>
+    )
+}
