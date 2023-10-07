cf41e86fd7e6ab05dbb5e4418b4bb3a80bb24f17
sripriya497 <sripriyadharshini497@gmail.com>
2023-10-07 16:10:00 -0500

    CSS changes

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 export default function Main(){
     return(
-        <h1>Main goes here</h1>
+        <main>
+            <h1 className='main--head'>Study Plan:</h1>
+            <ul className='main--list'>
+                <li>Front End Development - HTML, CSS, JS, React</li>
+                <li>Product Management - Use cases and hackathons</li>
+                <li>Backend Development - DSA and Leetcode</li>
+                <li>Cloud Engineer - AWS and labs</li>
+                <li>Project Management - SAPM and Usecases</li>
+            </ul>
+        </main>
     )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Navbar(){
     return(
         <nav>
-            <img src="../../image/React-icon.svg" width='50px' />
-            <h3>ReactFacts</h3>
-            <h4>Project 1</h4>
+            <img src="../../image/React-icon.svg" width='50px' className='nav--logo' />
+            <h3 className='nav--title'>sri-priya-spd</h3>
+            <h4 className='nav--context'>MS ITM </h4>
         </nav>
     )
 }
